ed5660fcc53df2bcaade561f68a8583764395456
Petr Janda <petrjanda@me.com>
2012-07-06 14:54:21 +0200

    fix typos

--- lib/watchr/analysers/flog.rb
+++ lib/watchr/analysers/flog.rb
@@ -5,13 +5,13 @@ module Watchr
     module Flog
       include SmellTypes
 
-      VERY_COMPLEX_OBJECT_TRASHOLD = 100
+      VERY_COMPLEX_OBJECT_THRESHOLD = 100
 
-      COMPLEX_OBJECT_TRASHOLD = 50
+      COMPLEX_OBJECT_THRESHOLD = 50
 
-      VERY_COMPLEX_METHOD_TRASHOLD = 40
+      VERY_COMPLEX_METHOD_THRESHOLD = 40
 
-      COMPLEX_METHOD_TRASHOLD = 20
+      COMPLEX_METHOD_THRESHOLD = 20
 
       def analyse_flog(report)
         report.classes.each do |klass|
@@ -37,18 +37,18 @@ module Watchr
         add_smell(
           get_smell_level?(target, type),
           target.name, target.location, target.total_score
-        ) if target.total_score >= get_trashold(:complex, type.upcase)
+        ) if target.total_score >= get_threshold(:complex, type.upcase)
       end
 
       def get_smell_level?(target, type)
-        trashold = get_trashold(:very_complex, type)
-        complexity = target.total_score >= trashold ? :very_complex : :complex
+        threshold = get_threshold(:very_complex, type)
+        complexity = target.total_score >= threshold ? :very_complex : :complex
 
         get_smell_type(complexity, type.upcase)
       end
 
-      def get_trashold(complexity, type)
-        Watchr::Analysers::Flog.const_get("#{complexity.upcase}_#{type.upcase}_TRASHOLD")
+      def get_threshold(complexity, type)
+        Watchr::Analysers::Flog.const_get("#{complexity.upcase}_#{type.upcase}_THRESHOLD")
       end
 
       def get_smell_type(complexity, type)
