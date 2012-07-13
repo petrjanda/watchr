bd423a11280cbfda71a9c08a9f52c05eaa12403c
Petr Janda <petrjanda@me.com>
2012-07-13 21:09:38 +0200

    filter smells

--- lib/watchr/analysers/reek.rb
+++ lib/watchr/analysers/reek.rb
@@ -12,10 +12,20 @@ module Watchr
             smell.location['lines'].first
           )
 
+          type = case smell.smell['subclass']
+          when 'IrresponsibleModule' then IRRESPONSIBLE_MODULE
+          when 'DuplicateMethodCall' then DUPLICATE_METHOD_CALL
+          when 'NestedIterators' then NESTED_ITERATORS
+          when 'TooManyStatements' then TOO_MANY_STATEMENTS
+          when 'FeatureEnvy' then FEATURE_ENVY
+          when 'UtilityFunction' then UTILITY_FUNCTION
+          else nil
+          end
+
           add_smell(Watchr::Smell.new(
             smell.smell['subclass'],
             smell.location['context'], smell.smell['message'], location, {}
-          ))
+          )) unless type.nil?
         end
       end
     end
--- lib/watchr/smell_types.rb
+++ lib/watchr/smell_types.rb
@@ -29,5 +29,28 @@ module Watchr
     # Code in multiple places is similar (not identical).
     #
     SIMILAR_CODE = :similar_code
+
+
+    IRRESPONSIBLE_MODULE = :irresponsible_module
+    DUPLICATE_METHOD_CALL = :duplicate_method_call
+    NESTED_ITERATORS = :nested_iterators
+    TOO_MANY_STATEMENTS = :too_many_statements
+    FEATURE_ENVY = :feature_envy
+    UTILITY_FUNCTION = :utility_function
+
+    ALL_SMELLS = [
+      COMPLEX_METHOD,
+      VERY_COMPLEX_METHOD,
+      COMPLEX_OBJECT,
+      VERY_COMPLEX_OBJECT,
+      IDENTICAL_CODE,
+      SIMILAR_CODE,
+      IRRESPONSIBLE_MODULE,
+      DUPLICATE_METHOD_CALL,
+      NESTED_ITERATORS,
+      TOO_MANY_STATEMENTS,
+      FEATURE_ENVY,
+      UTILITY_FUNCTION
+    ]
   end
 end
