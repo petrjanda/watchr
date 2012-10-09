1cba54e10ed10f6ee55e814a7754fd55d136a4ed
Petr Janda <petrjanda@me.com>
2012-10-09 09:31:27 +0200

    better spec

--- spec/watchr/analyse_spec.rb
+++ spec/watchr/analyse_spec.rb
@@ -8,7 +8,13 @@ describe Watchr::Analyse do
     subject { analyse.files }
 
     its(:count) { should == 2 }
-    its(:first) { subject.class.should == Watchr::FileAnalyse }
+
+    describe 'first' do
+      subject { analyse.files.first }
+    
+      its(:class) { should == Watchr::FileAnalyse }
+      its(:path) { should == 'spec/fixtures/class.rb' }
+    end
   end
 
   describe '#path' do
